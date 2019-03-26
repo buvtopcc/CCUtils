2d9bb40580a961fd57fb3a4ff949d11baddf9fcf
pengchangcheng <pengchangcheng@yy.com>
2019-03-26 11:26:49 +0800

    Initial commit

---
+++ Example/CCUtils/ccAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ccAppDelegate.h
+//  CCUtils
+//
+//  Created by pcc869@gmail.com on 03/26/2019.
+//  Copyright (c) 2019 pcc869@gmail.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ccAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/CCUtils/ccViewController.m
@@ -0,0 +1,29 @@
+//
+//  ccViewController.m
+//  CCUtils
+//
+//  Created by pcc869@gmail.com on 03/26/2019.
+//  Copyright (c) 2019 pcc869@gmail.com. All rights reserved.
+//
+
+#import "ccViewController.h"
+
+@interface ccViewController ()
+
+@end
+
+@implementation ccViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
